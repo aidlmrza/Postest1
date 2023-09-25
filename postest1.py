91331e60069d908711718e59a97ed72d4dbbfef6
Muhammad Aidil Mirza <144346363+aidlmrza@users.noreply.github.com>
2023-09-25 17:42:52 +0800

    postest1.py

--- postest1.py
+++ postest1.py
@@ -1,7 +1,12 @@
 import math
 
-print ("Selamat Datang di Kalkulator Bangun ruang")
 
+print ("Login dulu coy")
+username = input ("Masukkan nama        : ")
+password = input ("Masukkan Password    : ")
+
+
+print (f"Halo {username}, Selamat Datang di Kalkulator Volume Bangun ruang")
 print ("="*30)
 print ("""PILIH BANGUN RUANG
     1. Bola
@@ -14,19 +19,28 @@ pi = math.pi
 if bangun == 1:
     print ("Menghitung Volume Bola")
     jari_jari = float (input("Masukkan jari-jari = "))
-    V_Bola = 4/3 * pi * jari_jari**3
-    print (f"Volume Bolanya adalah {V_Bola}")
+    if jari_jari < 1:
+        print ("jari-jari harus lebih dari 1 coy")
+    else:
+        V_Bola = 4/3 * pi * jari_jari**3
+        print (f"Volume Bolanya adalah {V_Bola}")
 elif bangun == 2:
     print ("Menghitung Volume Tabung")
     jari_jari = float (input("Masukkan jari-jari = "))
     tinggi = float (input("Masukkan tinggi = "))
-    V_Tabung = pi * jari_jari**2 * tinggi
-    print (f"Volume tabungnya adalah {V_Tabung}")
+    if jari_jari < 1 or tinggi < 1:
+        print ("jari-jari sama tinggi ga boleh kurang dari 1 coy")
+    else:
+        V_Tabung = pi * jari_jari**2 * tinggi
+        print (f"Volume tabungnya adalah {V_Tabung}")
 elif bangun == 3:
     print ("Menghitung Volume Limas Segitiga")
     luas_alas = float (input("Masukkan luas alas = " ))
     tinggi = float (input("Masukkan tinggi = "))
-    V_LimasSegitiga = 1/3 * luas_alas * tinggi
-    print (f"Volume Limas Segitiganya adalah {V_LimasSegitiga}")
+    if luas_alas < 1 or tinggi < 1:
+        print ("luas alas sama tinggi ga boleh kurang dari 1 coy")
+    else:
+        V_LimasSegitiga = 1/3 * luas_alas * tinggi
+        print (f"Volume Limas Segitiganya adalah {V_LimasSegitiga}")
 else:
     print("hanya ada 3 pilihan")
